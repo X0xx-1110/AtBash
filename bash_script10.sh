41035de9cc446eed4df3e56a8321454358d0d535
X0xx-1110 <root@kalata.kalata>
2020-06-18 17:16:46 +0300

    Security basic check of Linuxgit  add bash_script10.shgit  add bash_script10.sh!

--- bash_script10.sh
+++ bash_script10.sh
@@ -3,28 +3,28 @@
 #Example: ./bash_script10.sh
 
 #/proc
-echo "\n/******************At LAST  day opened files at /proc ********************/\n" & \
+echo "\n/******************At LAST  day opened files over /proc ********************/\n" & \
 find /proc -type f -ctime 1 -mtime 1 -ls -true
 
 #/tmp
-echo "\n/******************At LAST day opened files at /tmp | executable | writable ********************/\n" & \
+echo "\n/******************At LAST day opened files over /tmp | executable | writable ********************/\n" & \
 find /tmp -type f -ctime 1 -mtime 1 -ls -true & \
 find /tmp -type f -ls -executable & \
 find /tmp -type f -ls -writable
 
 #/var
-echo "\n/******************At LAST day opened files at /var ********************/\n" & \
+echo "\n/******************At LAST day opened files over /var ********************/\n" & \
 find /var -type f -ctime 1 -mtime 1 -ls -true
 
 
 #/media
-echo "\n/******************At LAST day opened files at /media | executable | writable ********************/\n" & \
+echo "\n/******************At LAST day opened files over /media | executable | writable ********************/\n" & \
 find /media -type f -ctime 1 -mtime 1 -ls -true & \
 find /media -type f -ls -executable & \
 find /media -type f -ls -writable
 
 #/mnt
-echo "\n/******************LAST A day opened files at /mnt | executable | writable ********************/\n" & \
+echo "\n/******************At LAST day opened files over /mnt | executable | writable ********************/\n" & \
 find /mnt -type f -ctime 1 -mtime 1 -ls -true & \
 find /mnt -type f -ls -executable & \
 find /mnt -type f -ls -writable
