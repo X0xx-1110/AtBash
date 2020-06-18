c0ef8b76bb3fc4538d9c4cb3b64cbdeca25199a9
X0xx-1110 <root@kalata.kalata>
2020-06-18 17:14:54 +0300

    Security basic check of Linux;!

--- bash_script10.sh
+++ bash_script10.sh
@@ -3,22 +3,22 @@
 #Example: ./bash_script10.sh
 
 #/proc
-echo "\n/******************LAST A day opened files at /proc ********************/\n" & \
+echo "\n/******************At LAST  day opened files at /proc ********************/\n" & \
 find /proc -type f -ctime 1 -mtime 1 -ls -true
 
 #/tmp
-echo "\n/******************LAST A day opened files at /tmp | executable | writable ********************/\n" & \
+echo "\n/******************At LAST day opened files at /tmp | executable | writable ********************/\n" & \
 find /tmp -type f -ctime 1 -mtime 1 -ls -true & \
 find /tmp -type f -ls -executable & \
 find /tmp -type f -ls -writable
 
 #/var
-echo "\n/******************LAST A day opened files at /var ********************/\n" & \
+echo "\n/******************At LAST day opened files at /var ********************/\n" & \
 find /var -type f -ctime 1 -mtime 1 -ls -true
 
 
 #/media
-echo "\n/******************LAST A day opened files at /media | executable | writable ********************/\n" & \
+echo "\n/******************At LAST day opened files at /media | executable | writable ********************/\n" & \
 find /media -type f -ctime 1 -mtime 1 -ls -true & \
 find /media -type f -ls -executable & \
 find /media -type f -ls -writable
