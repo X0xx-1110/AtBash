65efa9578d60b90ba92ce3cb435d5cdc9fd12f47
X0xx-1110 <root@kalata.kalata>
2020-06-22 11:33:09 +0300

    URL one word Suggestions & BASIC and Common explanation!

---
+++ bash_script13.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#URL one word Suggestions & BASIC and Common explanation!
+#Example ./bash_script13.sh flower
+
+syn=$(wn ${1} -coorv | grep \=\> | grep -v \, | cut -d ">" -f 2 | sort | uniq | head -50)
+if [[ ${#syn} -eq 0 ]]
+then
+	echo "NO RESULT!!!"
+fi
+
+for i in $(echo $syn)
+do
+	t1=$(host -t a www."${i}".com | grep -bo "not found")  > /dev/null
+	if [[ ${t1%%\:} && $(curl --output /dev/null --silent --head --fail) ]]
+	then
+		echo ""
+	else
+		echo "|- http://www."${i}".com"
+	fi 
+done
