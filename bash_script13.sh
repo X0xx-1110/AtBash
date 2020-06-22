0c120914171c981c44b2e4f7dbfe42b6a5a5334f
X0xx-1110 <root@kalata.kalata>
2020-06-22 16:19:26 +0300

    NEW version & URL one word Suggestions & BASIC and Common explanation!

--- bash_script13.sh
+++ bash_script13.sh
@@ -2,15 +2,14 @@
 #URL one word Suggestions & BASIC and Common explanation!
 #Example ./bash_script13.sh flower
 
-syn=$(wn ${1} -coorv | grep \=\> | grep -v \, | cut -d ">" -f 2 | sort | uniq | head -50)
+syn=$(wn ${1} -coorv | grep \=\> | grep -v \, | cut -d ">" -f 2 | sort | uniq | head -50) > /dev/null
 if [[ ${#syn} -eq 0 ]]
 then
 	echo "NO RESULT!!!"
-fi
-
+else
 for i in $(echo $syn)
 do
-	t1=$(host -t a www."${i}".com | grep -bo "not found")  > /dev/null
+	t1=$(host -t a www."${i}".com | grep -vbo "not found")  > /dev/null
 	if [[ ${t1%%\:} && $(curl --output /dev/null --silent --head --fail) ]]
 	then
 		echo ""
@@ -18,3 +17,4 @@ do
 		echo "|- http://www."${i}".com"
 	fi 
 done
+fi
