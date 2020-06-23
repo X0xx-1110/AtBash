b2b8645bb1952f1207adc0e524aa7369cb422e46
X0xx-1110 <root@kalata.kalata>
2020-06-23 12:43:51 +0300

    Security + add uuid

---
+++ bash_script15.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#A little bit more of these! & My point of view.
+
+uuidd -p /tmp/uuidd.pid -s /tmp/uuidd.socket && uuidd -d -r -n 1012312307 -s /tmp/uuidd.socket && uuidd -d -k -s /tmp/uuidd.socket
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
