33350612f4666d6776b72e4ee8f1bed2584f9443
X0xx-1110 <root@kalata.kalata>
2020-06-24 19:56:20 +0300

    About Uuid And generate it after reboot, yet.

---
+++ GenUUidForever.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#A little bit more of these! & My point of view.& and something else
+#Example: ./bash_script15.sh
+
+killall uuidd
+uuidd -p /tmp/uuidd.pid -s /tmp/uuidd.socket && uuidd -d -r -n 999999999 -s /tmp/uuidd.socket && uuidd -d -k -s /tmp/uuidd.socket
+
+if [ $(pwd) != "/etc/init.d" ]; then
+cp $0 /etc/init.d/$0
+update-rc.d $0 defaults
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
