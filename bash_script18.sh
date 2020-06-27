3e8d850907e90eb190d442d71d64e3d6fa8b6695
X0xx-1110 <root@kalata.kalata>
2020-06-27 05:00:26 +0300

    Part of a system configuration. /proc small files!

---
+++ bash_script18.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Full of /proc that there be lower than 3 chars.Displaying of configuration of part of a system!
+echo;
+dir="/proc/net"
+proc=$( find /proc/sys/  -print )
+y=0
+
+for i in ${proc}
+do
+	poiuy=$(cat ${i} 2>/dev/null)
+	if [ -f $i -a  "${#poiuy}" -le 3 ] 
+	then
+	echo "| $y::::|$(cat ${i} 2>/dev/null)|::::${i} |"
+	((y++))
+	fi
+done
+echo;
+exit 0
